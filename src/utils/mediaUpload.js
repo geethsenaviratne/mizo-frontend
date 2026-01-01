7a7adfdd4508114d322e525facbe9766c8f05839
Geeth Seneviratne <it23226128@my.sliit.lk>
2026-01-01 10:55:56 +0530

    Add media upload functionality to Supabase 📂

---
+++ src/utils/mediaUpload.js
@@ -0,0 +1,34 @@
+import { createClient } from "@supabase/supabase-js";
+
+const key = "sb_publishable_AgzFw9-9lT_PgmnDllcRig_MA3BuQaD"
+const url ="https://fzhqzgghgyytjbginpdb.supabase.co"
+
+export default function uploadMediaToSupabase(file) {
+
+    return new Promise ((resolve, reject) => {
+        if (file == null) {
+            reject("No file provided")
+        }
+        let fileName = file.name;
+        const extension = fileName.split('.')[fileName.split('.').length - 1]
+
+        const supabase = createClient(url, key);
+
+        const timestamp = new Date().getTime();
+
+        fileName = timestamp + file.name + '.' + extension;
+
+        supabase.storage.from("images").upload(fileName, file, {
+            cacheControl: "3600",
+            upsert: false
+        })
+        .then(()=> {
+            const publicUrl = supabase.storage.from("images").getPublicUrl(fileName).data.publicUrl;
+            resolve(publicUrl);
+        }).catch((error) => {
+            reject(error.message);
+        })
+
+
+})
+}
